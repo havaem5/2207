a8ccd4aaf1e073bc5462af19d9ecee7e68ca5bb1
havaem <vhnvohoainam@gmail.com>
2022-07-22 14:41:20 +0700

    hi

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	env: {
-		API_URL: "https://zodinet-1807.herokuapp.com",
+		API_URL: "https://zodinet-2207.herokuapp.com",
 		RECAPTCHA_SECRET: "6LfgfwohAAAAACPQHMW1jeCoGB4wLBJ9GtZgpBp1",
 		RECAPTCHA_PUBLIC: "6LfgfwohAAAAAKY_UPzJq5KY-LFDnl4vjW61X5Xf",
 	},
